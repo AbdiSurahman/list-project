3fafba093477df189d89833930ec53744e3b72b4
abdisurahman <abdisurahman27@gmail.com>
2023-06-26 21:16:33 +0800

    Develop task create

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -33,6 +33,27 @@ class TaskController extends Controller
     ]);
     }
 
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'name' => 'required',
+                'due_date' => 'required',
+                'status' => 'required',
+            ],
+            $request->all()
+        );
+        
+        Task::create([
+            'name' => $request->name,
+            'detail' => $request->detail,
+            'due_date' => $request->due_date,
+            'status' => $request->status,
+        ]);
+
+        return redirect()->route('tasks.index');
+    }
+
     public function edit($id)
     {
         $pageTitle = 'Edit Task';
