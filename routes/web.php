714f7c243bbf48880e9b626be27712f38ae51c97
abdisurahman <abdisurahman27@gmail.com>
2023-07-10 18:41:16 +0800

    Develop task delete

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::prefix('tasks')
         Route::post('/', 'store')->name('store');
         Route::get('{id}/edit', 'edit')->name('edit');
         Route::put('{id}/update', 'update')->name('update');
+        Route::get('{id}/delete', 'delete')->name('delete');
+        Route::delete('{id}/destroy', 'destroy')->name('destroy');
     });
 
 // Route::get('/tasks/', [TaskController::class, 'index'])->name('tasks.index');
